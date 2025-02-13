a33c7410b669a0517b00b3f2c6c1bfebc1268612
Chintulga <chintulast@gmail.com>
2025-02-14 03:52:21 +0800

    build err

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,48 @@ import Image5 from "@/assets/images/BG (6).png";
 import { Badge, BadgeType } from "@/components/badge";
 import { AddBagButton } from "@/components/actions/add-bag";
 
+export type VariantType = {
+  name: string;
+  color: string;
+};
+
+export const product = {
+  id: "1",
+  name: "Product Name",
+  brand: {
+    id: "1",
+    name: "Brand Name",
+  },
+  images: [Image1.src, Image2.src, Image3.src, Image4.src, Image5.src],
+  variants: [
+    { name: "Seafoam", color: "bg-[#3467C2]" },
+    { name: "Wine", color: "bg-[#713A41]" },
+    { name: "Sunflower", color: "bg-[#EED34E]" },
+    { name: "Ash", color: "bg-[#8C8B7D]" },
+    { name: "Teal", color: "bg-[#3B8589]" },
+    { name: "Mint", color: "bg-[#AAC584]" },
+    { name: "Sand", color: "bg-[#EDEAB8]" },
+  ],
+  sizes: [
+    { size: "36", stock: 5 },
+    { size: "37", stock: 0 },
+    { size: "38", stock: 10 },
+    { size: "39", stock: 3 },
+    { size: "40", stock: 8 },
+  ],
+  description:
+    "This turtle neck sweater has a relaxed fit and timeless color. An engaging design twist has been applied with asymmetric stitch details. Elegant yet classic and trendy with its own design feature. This piece can be tucked into everything from tailoring to faded jeans or a leather midi skirt.",
+  productDetails: [
+    { name: "neckType", description: "T-neck" },
+    { name: "material", description: "100% Mongolian cashmere" },
+    { name: "weight", description: "525 grams" },
+    { name: "ply", description: "8 ply" },
+    { name: "gauge", description: "3.5 gauge" },
+    { name: "stitchTypes", description: "Cable stitch, Fisherman rib stitch" },
+    { name: "fit", description: "Loose fit" },
+  ],
+};
+
 export default function ProductPage() {
   const { images, variants, sizes } = product;
 
@@ -90,45 +132,3 @@ export default function ProductPage() {
     </>
   );
 }
-
-export type VariantType = {
-  name: string;
-  color: string;
-};
-
-export const product = {
-  id: "1",
-  name: "Product Name",
-  brand: {
-    id: "1",
-    name: "Brand Name",
-  },
-  images: [Image1.src, Image2.src, Image3.src, Image4.src, Image5.src],
-  variants: [
-    { name: "Seafoam", color: "bg-[#3467C2]" },
-    { name: "Wine", color: "bg-[#713A41]" },
-    { name: "Sunflower", color: "bg-[#EED34E]" },
-    { name: "Ash", color: "bg-[#8C8B7D]" },
-    { name: "Teal", color: "bg-[#3B8589]" },
-    { name: "Mint", color: "bg-[#AAC584]" },
-    { name: "Sand", color: "bg-[#EDEAB8]" },
-  ],
-  sizes: [
-    { size: "36", stock: 5 },
-    { size: "37", stock: 0 },
-    { size: "38", stock: 10 },
-    { size: "39", stock: 3 },
-    { size: "40", stock: 8 },
-  ],
-  description:
-    "This turtle neck sweater has a relaxed fit and timeless color. An engaging design twist has been applied with asymmetric stitch details. Elegant yet classic and trendy with its own design feature. This piece can be tucked into everything from tailoring to faded jeans or a leather midi skirt.",
-  productDetails: [
-    { name: "neckType", description: "T-neck" },
-    { name: "material", description: "100% Mongolian cashmere" },
-    { name: "weight", description: "525 grams" },
-    { name: "ply", description: "8 ply" },
-    { name: "gauge", description: "3.5 gauge" },
-    { name: "stitchTypes", description: "Cable stitch, Fisherman rib stitch" },
-    { name: "fit", description: "Loose fit" },
-  ],
-};
